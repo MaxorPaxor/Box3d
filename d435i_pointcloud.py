758c8e699d55c597c062e8b0910911c96031ddca
Max <m4x.mona@gmail.com>
2021-08-03 17:30:49 +0300

    02.08.21

--- d435i_pointcloud.py
+++ d435i_pointcloud.py
@@ -21,7 +21,9 @@ class Measure:
         self.device = self.pipeline_profile.get_device()
         self.device_product_line = str(self.device.get_info(rs.camera_info.product_line))
 
+        # self.Wd, self.Hd = 1280, 720
         self.Wd, self.Hd = 640, 480
+        # self.Wd, self.Hd = 1024, 768
         if self.device_product_line == 'L500':
             self.Wc, self.Hc = 960, 540
         else:
@@ -88,6 +90,11 @@ class Measure:
         self.draw = False
 
     def click(self, event, x, y, flags, param):
+        """
+        Click function. A paint-like brush tool to color an object
+        and collect all of its pixels including depth information
+        to the global buffer of the class.
+        """
 
         if event == cv2.EVENT_LBUTTONDOWN:
             self.draw = True
@@ -97,36 +104,44 @@ class Measure:
                 if x >= self.Wf:
                     x -= self.Wf
 
-                click_area = 3
-                scatter_factor = 6
+                click_area = 4
+                scatter_factor = 4
 
                 if len(self.points_buffer) > 0:
                     for i in range(-click_area+1, click_area):
                         for j in range(-click_area+1, click_area):
 
                             if np.sum([v['coords'] == (x + i*scatter_factor, y + j*scatter_factor) for v in self.points_buffer.values()]) == 0:
+                                # Don't add new points if they exists in the buffer
 
                                 dist = self.depth_frame.get_distance(x+i*scatter_factor, y+j*scatter_factor)
-                                self.points_buffer[len(self.points_buffer)] = {'coords': (x+i*scatter_factor, y+j*scatter_factor),
-                                                                               'dist': dist}
 
-                                tmp_3d = self.get_3d_coords(self.points_buffer[len(self.points_buffer) - 1])
-                                self.points_3d_buffer_list.append(tmp_3d)
+                                if dist != 0:
+                                    self.points_buffer[len(self.points_buffer)] = {'coords': (x+i*scatter_factor, y+j*scatter_factor),
+                                                                                   'dist': dist}
+
+                                    tmp_3d = self.get_3d_coords(self.points_buffer[len(self.points_buffer) - 1])
+                                    self.points_3d_buffer_list.append(tmp_3d)
 
                 else:
                     for i in range(-click_area+1, click_area):
                         for j in range(-click_area+1, click_area):
                             dist = self.depth_frame.get_distance(x + i*scatter_factor, y + j*scatter_factor)
-                            self.points_buffer[len(self.points_buffer)] = {'coords': (x + i*scatter_factor, y + j*scatter_factor),
-                                                                           'dist': dist}
 
-                            tmp_3d = self.get_3d_coords(self.points_buffer[len(self.points_buffer) - 1])
-                            self.points_3d_buffer_list.append(tmp_3d)
+                            if dist != 0:
+                                self.points_buffer[len(self.points_buffer)] = {'coords': (x + i*scatter_factor, y + j*scatter_factor),
+                                                                               'dist': dist}
+
+                                tmp_3d = self.get_3d_coords(self.points_buffer[len(self.points_buffer) - 1])
+                                self.points_3d_buffer_list.append(tmp_3d)
 
         elif event == cv2.EVENT_LBUTTONUP:
             self.draw = False
 
     def get_smart_dist(self, x, y):
+        """
+        Uses smart method to calculate distance. Based on neighbourhood of a single pixel.
+        """
         calc_dist_reg = 10
         d_mat = np.zeros((2 * calc_dist_reg - 1, 2 * calc_dist_reg - 1))
         for i in range(-calc_dist_reg, calc_dist_reg):
@@ -147,30 +162,30 @@ class Measure:
         if len(self.inter_points) > 0:
             i = 1
             for point in self.inter_points:
-                cv2.circle(self.images, point,
+                cv2.circle(self.images, (point[0], point[1]),
                            4, (20, 200, 20), -1)  # Left image
 
                 cv2.putText(self.images,
                             "{}".format(i),
-                            point,
+                            (point[0], point[1]),
                             self.font, 0.6, (20, 20, 200), 1)  # Left image
                 i += 1
 
             # Box lines - Left image
-            cv2.line(self.images, self.inter_points[0], self.inter_points[1], (20, 200, 20), 1)  # 1-2
-            cv2.line(self.images, self.inter_points[0], self.inter_points[2], (20, 200, 20), 1)  # 1-3
-            cv2.line(self.images, self.inter_points[1], self.inter_points[3], (20, 200, 20), 1)  # 2-4
-            cv2.line(self.images, self.inter_points[2], self.inter_points[3], (20, 200, 20), 1)  # 3-4
+            cv2.line(self.images, tuple(self.inter_points[0]), tuple(self.inter_points[1]), (20, 200, 20), 1)  # 1-2
+            cv2.line(self.images, tuple(self.inter_points[0]), tuple(self.inter_points[2]), (20, 200, 20), 1)  # 1-3
+            cv2.line(self.images, tuple(self.inter_points[1]), tuple(self.inter_points[3]), (20, 200, 20), 1)  # 2-4
+            cv2.line(self.images, tuple(self.inter_points[2]), tuple(self.inter_points[3]), (20, 200, 20), 1)  # 3-4
 
-            cv2.line(self.images, self.inter_points[4], self.inter_points[5], (20, 200, 20), 1)  # 5-6
-            cv2.line(self.images, self.inter_points[4], self.inter_points[6], (20, 200, 20), 1)  # 5-7
-            cv2.line(self.images, self.inter_points[5], self.inter_points[7], (20, 200, 20), 1)  # 6-8
-            cv2.line(self.images, self.inter_points[6], self.inter_points[7], (20, 200, 20), 1)  # 7-8
+            cv2.line(self.images, tuple(self.inter_points[4]), tuple(self.inter_points[5]), (20, 200, 20), 1)  # 5-6
+            cv2.line(self.images, tuple(self.inter_points[4]), tuple(self.inter_points[6]), (20, 200, 20), 1)  # 5-7
+            cv2.line(self.images, tuple(self.inter_points[5]), tuple(self.inter_points[7]), (20, 200, 20), 1)  # 6-8
+            cv2.line(self.images, tuple(self.inter_points[6]), tuple(self.inter_points[7]), (20, 200, 20), 1)  # 7-8
 
-            cv2.line(self.images, self.inter_points[0], self.inter_points[4], (20, 200, 20), 1)  # 1-5
-            cv2.line(self.images, self.inter_points[1], self.inter_points[5], (20, 200, 20), 1)  # 2-6
-            cv2.line(self.images, self.inter_points[2], self.inter_points[6], (20, 200, 20), 1)  # 3-7
-            cv2.line(self.images, self.inter_points[3], self.inter_points[7], (20, 200, 20), 1)  # 4-8
+            cv2.line(self.images, tuple(self.inter_points[0]), tuple(self.inter_points[4]), (20, 200, 20), 1)  # 1-5
+            cv2.line(self.images, tuple(self.inter_points[1]), tuple(self.inter_points[5]), (20, 200, 20), 1)  # 2-6
+            cv2.line(self.images, tuple(self.inter_points[2]), tuple(self.inter_points[6]), (20, 200, 20), 1)  # 3-7
+            cv2.line(self.images, tuple(self.inter_points[3]), tuple(self.inter_points[7]), (20, 200, 20), 1)  # 4-8
 
             cv2.putText(self.images, "Distance 1-5: {:.2f} mm".format(round(abs(self.planes[0][3] - self.planes[3][3])*1000, 3)),
                         (10, 20), self.font, 0.6, (20, 200, 20), 1)  # Left image
@@ -299,7 +314,6 @@ class Measure:
             for k in range(len(self.points_buffer)):
                 X, Y, Z = self.get_3d_coords(self.points_buffer[k])
                 error = np.dot(plane, np.array([X, Y, Z, 1]))
-                # print(error)
                 if abs(error) < accepted_error:
                     inliers.append(self.points_buffer[k])
 
@@ -336,8 +350,7 @@ class Measure:
 
         return best_plane
 
-    @staticmethod
-    def calc_2plane_inter(plane1, plane2):
+    def calc_2plane_inter(self, plane1, plane2):
         """
         Returns intersection line between two planes
         :param plane1: [A, B, C, D]
@@ -404,26 +417,18 @@ class Measure:
         min_proj = np.min(proj)
         avg_proj = np.average(proj)
 
-        # diff = []
-        # for i in range(len(proj)-1):
-        #     diff.append(proj[i+1] - proj[i])
-        #     print(diff[-1])
-        #
-        # avg_diff = np.average(diff)
-        # print("AVG DIFF: {}".format(avg_diff))
-
-        print(np.histogram(proj, bins=100))
+        # print(np.histogram(proj, bins=100))
 
         new_plane = plane[:3]
         print("max: {}, min: {}, AVG: {}".format(max_proj, min_proj, avg_proj))
         if abs(max_proj - plane[3]) < abs(min_proj - plane[3]):
-            # new_plane.append(min_proj)
-            new_plane.append(proj[int(0.005 * len(proj))])
+            new_plane.append(min_proj)
+            # new_plane.append(proj[int(0.005 * len(proj))])
             print(
                 "Adding new plane. min proj: {}, custom proj : {}".format(min_proj, proj[int(0.005 * len(proj))]))
         else:
-            # new_plane.append(max_proj)
-            new_plane.append(proj[int(0.995 * len(proj))])
+            new_plane.append(max_proj)
+            # new_plane.append(proj[int(0.995 * len(proj))])
             print(
                 "Adding new plane. max proj: {}, custom proj : {}".format(max_proj, proj[int(0.995 * len(proj))]))
 
@@ -431,8 +436,22 @@ class Measure:
 
         return proj
 
-    def keyboard_clicks(self):
+    def display_inlier_outlier(self, cloud, ind):
+        """
+        Open3d helper function to draw outliers and inliers
+        """
+        inlier_cloud = cloud.select_by_index(ind)
+        outlier_cloud = cloud.select_by_index(ind, invert=True)
 
+        print("Showing outliers (red) and inliers (gray): ")
+        outlier_cloud.paint_uniform_color([1, 0, 0])
+        inlier_cloud.paint_uniform_color([0.8, 0.8, 0.8])
+        o3d.visualization.draw_geometries([inlier_cloud, outlier_cloud])
+
+    def keyboard_clicks(self):
+        """
+        Keyboard key presses during main loop
+        """
         key = cv2.waitKey(1)
 
         if key == ord("q"):  # q quits the program
@@ -453,15 +472,46 @@ class Measure:
             else:
                 self.pause_frame = True
 
-        if key == ord("c"):  # calculate cuboid
+        if key == ord("c"):  # calculate cuboid from buffer points
             cube = pyrsc.Cuboid()
-            best_eq, best_inliers = cube.fit(np.array(self.points_3d_buffer_list), thresh=3*10**-3, maxIteration=10000)
+            best_eq, best_inliers = cube.fit(np.array(self.points_3d_buffer_list), thresh=2e-2, maxIteration=10000)
             self.planes.append(best_eq.tolist())
             self.planes = self.planes[0]
 
             pcd = o3d.geometry.PointCloud()
             pcd.points = o3d.utility.Vector3dVector(np.array(self.points_3d_buffer_list))
-            pcd_good = pcd.select_by_index(best_inliers)
+            pcd_good_ransac = pcd.select_by_index(best_inliers)
+
+            ### Filtering
+            # cl, ind = pcd_good.remove_radius_outlier(nb_points=len(best_inliers), radius=1)  # Radial filter
+            cl, ind = pcd_good_ransac.remove_statistical_outlier(nb_neighbors=int(1*len(best_inliers)),
+                                                          std_ratio=2.0)  # Statistical filter
+            # self.display_inlier_outlier(pcd_good_ransac, ind)
+            pcd_good = pcd_good_ransac.select_by_index(ind)
+
+            ### Clustering
+            # with o3d.utility.VerbosityContextManager(
+            #         o3d.utility.VerbosityLevel.Debug) as cm:
+            #     labels = np.array(
+            #         pcd_good.cluster_dbscan(eps=0.1, min_points=100, print_progress=True))
+            # max_label = labels.max()
+            # print(f"point cloud has {max_label + 1} clusters")
+            # colors = plt.get_cmap("tab20")(labels / (max_label if max_label > 0 else 1))
+            # colors[labels < 0] = 0
+            # pcd_good.colors = o3d.utility.Vector3dVector(colors[:, :3])
+            # o3d.visualization.draw_geometries([pcd_good])
+
+            # pcd_good.colors = o3d.utility.Vector3dVector(np.random.uniform(0, 1, size=(2000, 3)))
+            # voxel_grid = o3d.geometry.VoxelGrid.create_from_point_cloud(pcd_good, voxel_size=0.005)
+            # o3d.visualization.draw_geometries([voxel_grid])
+            # octree = o3d.geometry.Octree(max_depth=8)
+            # octree.create_from_voxel_grid(voxel_grid)
+            # o3d.visualization.draw_geometries([octree])
+
+            # octree = o3d.geometry.Octree(max_depth=4)
+            # octree.convert_from_point_cloud(pcd_good, size_expand=0.01)
+            # o3d.visualization.draw_geometries([octree])
+
             points = np.asarray(pcd_good.points)
 
             self.add_opposite_plane(self.planes[0], points)
@@ -475,14 +525,19 @@ class Measure:
                                                        round(abs(self.planes[2][3] - self.planes[5][3])*1000, 2))
                   )
 
-            pcd_bbox = pcd = o3d.geometry.PointCloud()
-            pcd_bbox.points = o3d.utility.Vector3dVector(np.array(self.inter_3d_points))
-            hull, _ = pcd_bbox.compute_convex_hull()
-            hull_ls = o3d.geometry.LineSet.create_from_triangle_mesh(hull)
-            hull_ls.paint_uniform_color((1, 0, 0))
-            o3d.visualization.draw_geometries([pcd_bbox, pcd_good, hull_ls], point_show_normal=True)
+            # pcd_bbox = o3d.geometry.PointCloud()
+            # pcd_bbox.points = o3d.utility.Vector3dVector(np.array(self.inter_3d_points))
+            # hull, _ = pcd_bbox.compute_convex_hull()
+            # hull_ls = o3d.geometry.LineSet.create_from_triangle_mesh(hull)
+            # hull_ls.paint_uniform_color((1, 0, 0))
+            # o3d.visualization.draw_geometries([pcd_bbox, pcd_good, hull_ls], point_show_normal=True)
 
     def run(self):
+        """
+        The main function in the class that runs the camera,
+        filters the obtained depth-image,
+        and calls for mouse-click and keyboard-clicks
+        """
 
         cv2.namedWindow("RealSense-1", cv2.WINDOW_AUTOSIZE)
         while not self.stop:
@@ -502,9 +557,9 @@ class Measure:
                 # Apply the filters
                 self.depth_frame = self.depth_to_disparity.process(self.depth_frame)
                 self.depth_frame = self.spatial.process(self.depth_frame)
-                self.depth_frame = self.temporal.process(self.depth_frame)
+                # self.depth_frame = self.temporal.process(self.depth_frame)
                 self.depth_frame = self.disparity_to_depth.process(self.depth_frame)
-                self.depth_frame = self.hole_filling.process(self.depth_frame)
+                # self.depth_frame = self.hole_filling.process(self.depth_frame)
                 self.depth_frame.__class__ = rs.depth_frame
 
             # Apply colormap on depth image and convert to numpy
